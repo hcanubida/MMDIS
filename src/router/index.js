81af604351a6e4e18f946b7f4d82f4ee387e9adc
Hannah Bennett Althea Canubida <hcanubida@gmail.com>
2024-03-22 17:28:12 +0800

    fix redirection of mtss ostc

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
-//router file
+// router file
 
 import { createRouter, createWebHistory } from "vue-router";
 import home from "../views/home.vue";
 import login from "../views/login.vue";
 import mtss_dashboard from "../views/mtss/mtss_dashboard.vue";
+import mtss_ostc from "../views/mtss/mtss_ostc.vue"; // Import mtss_ostc component
 
 const routes = [
     { path: "/", component: home },
     { path: "/login", component: login },
     { path: "/mtss/mtss_dashboard", component: mtss_dashboard },
-    // { path: "/mtss/mtss_ostc", component: mtss_ostc },
+    { path: "/mtss/mtss_ostc", component: mtss_ostc }, // Use mtss_ostc component here
 ];
 
 const router = createRouter({
