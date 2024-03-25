6e27764c26e1347c1ea91850052d630cae22c0a2
Hannah Bennett Althea Canubida <hcanubida@gmail.com>
2024-03-25 16:11:59 +0800

    .

--- src/components/login-btn.vue
+++ src/components/login-btn.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="h-24 mr-2 top-0 flex fixed w-full justify-end">
     <div class="flex justify-end mr-3 items-center">
-      <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" class="text-dark bg-orange-200 hover:bg-red-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm pl-4 pr-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center" @click="redirectToLoginPage">Log in <img class="w-2 m-2" src="../assets/icons/drop-down.png"></button>
+      <button id="hs-dropdown-default" type="button" class="text-dark bg-orange-200 hover:bg-red-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm pl-4 pr-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center" @click="redirectToLoginPage">Log in <img class="w-2 m-2" src="../assets/icons/drop-down.png"></button>   
     </div>
+    
+    
   </div>
 </template>
 
