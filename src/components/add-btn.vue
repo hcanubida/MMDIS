cd7f8cee57444d0b35cea579a0383c1f027e4f77
Hannah Bennett Althea Canubida <hcanubida@gmail.com>
2024-03-25 14:09:36 +0800

    ostc

---
+++ src/components/add-btn.vue
@@ -0,0 +1,10 @@
+<template>
+      <div class="flex justify-end mt-2 mr-3">
+        <button class="text-dark bg-orange-200 hover:bg-red-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm pl-4 pr-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center">Add Request<img class="w-4 m-2" src="../assets/icons/plus.png"></button>
+      </div>
+</template>
+  
+  <script>
+    import '../assets/login-btn.css'
+    import router from '../router/index.js';
+  </script>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,15 @@
 <!-- header.vue -->
 
 <template>
-    <div class="header-container">
-      <div id="header" class=" border-gray-200 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div id="header" class="overflow-hidden h-24 fixed w-full flex flex-row flex-wrap top-0 left-0 border-gray-200 lg:px-6 py-2.5 dark:bg-gray-800 fixed w-full flex flex-row flex-wrap top-0 left-0">
         <img src="../assets/icons/logo.png" alt="MGBLogo" id="logo" @click="goToDashboard">
-        <div id="header-text" class="md:shrink-0">
+        <div class="md:shrink-0 align-middle self-center">
           <p class="leading-tight text-base text-gray-900 dark:text-white ml-2">Republic of the Philippines</p>
           <p class="leading-tight text-base text-gray-900 dark:text-white ml-2">Department of Environment and Natural Resources</p>
           <p class="leading-tight text-lg font-bold text-gray-900 dark:text-white ml-2">Mines and Geosciences Bureau - Region X</p>
           <p class="leading-tight text-base text-gray-900 dark:text-white ml-2">DENR-X Compound, Puntod, Cagayan de Oro City</p>
         </div> 
       </div>
-    </div>
 </template>
   
   <script>
