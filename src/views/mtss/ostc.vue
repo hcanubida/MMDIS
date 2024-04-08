dd32418a01ad488379e40b875bdfc463fc4c8e64
Hannah Bennett Althea Canubida <hcanubida@gmail.com>
2024-04-08 14:26:57 +0800

    .

--- src/views/mtss/ostc.vue
+++ src/views/mtss/ostc.vue
@@ -69,47 +69,16 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                Silver
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
-                            </th>
-                            <td class="px-6 py-4">
-                                White
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop PC
-                            </td>
-                            <td class="px-6 py-4">
-                                $1999
-                            </td>
-                        </tr>
-                        <tr class="bg-white dark:bg-gray-800">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td class="px-6 py-4">
-                                Black
-                            </td>
-                            <td class="px-6 py-4">
-                                Accessories
-                            </td>
-                            <td class="px-6 py-4">
-                                $99
-                            </td>
+                        <tr v-for="(ostc, index) in mtssOSTC" :key="index" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="px-6 py-4">{{ ostc.client }}</td>
+                            <td class="px-6 py-4">{{ ostc.certnum }}</td>
+                            <td class="px-6 py-4">{{ ostc.dateReceivedByORD }}</td>
+                            <td class="px-6 py-4">{{ ostc.dateReceivedByMMD }}</td>
+                            <td class="px-6 py-4">{{ ostc.PaymentDate }}</td>
+                            <td class="px-6 py-4">{{ ostc.sampleInspection }}</td>
+                            <td class="px-6 py-4">{{ ostc.dateIssued }}</td>
+                            <td class="px-6 py-4">{{ ostc.MMDPersonnel }}</td>
+                            <td class="px-6 py-4">{{ ostc.ProofOfMOV }}</td>
                         </tr>
                     </tbody>
                 </table>
